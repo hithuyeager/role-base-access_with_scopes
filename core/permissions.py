c3c7cd5327a2247d4913ae9dbabded4ce22e660b
hithesh <hitheshrhithu41@gmail.com>
2026-04-13 14:49:20 +0000

    initialized scopes

---
+++ core/permissions.py
@@ -0,0 +1,15 @@
+ROLE_SCOPES = {
+    "viewer" : [
+        "posts:read"
+    ],
+    "editor" : [
+        "posts:read",
+        "posts:write"
+    ],
+    "admin" : [
+        "posts:read",
+        "posts:write",
+        "posts:delete"
+        "users:read"
+    ]
+}
